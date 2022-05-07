18906d9321615837df2f5060efceadc2f89d0e81
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-05-06 20:06:38 -0400

    Update requirement installer.bat

--- requirement installer.bat
+++ requirement installer.bat
@@ -1,3 +1,3 @@
-curl "https://raw.githubusercontent.com/YumYummity/Discord-RAT/main/ignore/requirements.txt" --output "requirements.txt"
+curl "https://raw.githubusercontent.com/moom825/Discord-RAT/main/ignore/requirements.txt" --output "requirements.txt"
 pip3 install -r requirements.txt
 del /f requirements.txt
